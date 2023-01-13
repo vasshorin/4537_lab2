f32bd6d26c442f9254a96490fd8454b75a076ce5
Vasily Shorin <86452814+vasshorin@users.noreply.github.com>
2023-01-13 11:43:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const cors = require('cors');
@@ -6,17 +6,11 @@ const cors = require('cors');
 const app = express();
 app.use(express.json()); // read JSON BODY
 app.use(express.urlencoded({ extended: true })); // read URL ecnoded body
-app.use(cors);
+app.use(cors());
 
-app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-	res.setHeader("Access-Control-Allow-Credentials", true);
-	next();
-  });
 
 app.post('/chatbot', (req, res) => {
+	res.headers("access-control-allow-origin", "*")
 	const message = req.body.message;
 	const number = message.match(/\d+/);
 	if (number) {
